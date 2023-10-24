325f6e58d0c419f000679b30c0b561fc6d3b49e7
Elwin BASSAGET <elwin.bassaget@data-tricks.fr>
2023-10-24 18:04:26 +0200

    first part of the project

---
+++ vue-crash-course/src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button @click="onClick()" text="Add Task" color="green"></Button>
+    </header>
+</template>
+
+<script>
+import Button from './Button.vue';
+export default {
+    name:'Header-component',
+    components: {
+        Button,
+    },
+    props: {
+        title: {
+            type: String,
+            default: 'Default title'
+        }
+    },
+    methods: {
+        onClick() {
+            console.log('click');
+        }
+    }}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+</style>
---
+++ vue-crash-course/src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-for="task in tasks" :key="task.id">
+        <h3> {{ task.text }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Tasks-component',
+    props: {
+        tasks: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
